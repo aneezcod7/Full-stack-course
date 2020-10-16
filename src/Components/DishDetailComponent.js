36d9ebcc85261455b1f06648496de61db6c4fb1a
Aneez Rahman <aneezcod@gmail.com>
2020-10-17 03:35:30 +0530

    Fetch from Server

--- src/Components/DishDetailComponent.js
+++ src/Components/DishDetailComponent.js
@@ -5,6 +5,7 @@ import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../Shared/baseUrl';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -12,7 +13,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
     function RenderDish({dish}) {
         return(
             <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
